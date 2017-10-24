a27f32d9fb8b96e7b57370bd554af38ef56068ab
tangdi <tangdi0802@foxmail.com>
2017-10-24 17:47:36 +0800

    Initial commit

---
+++ src/DispatcherServlet.java
@@ -0,0 +1,31 @@
+
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class DispatcherServlet extends GenericServlet{
+
+    private String userName;
+
+    private String password;
+
+    private ServletContext servletContext;
+
+    private String tagjsp = "/hello.jsp";
+
+    public static final String USE = "use";
+
+    public static final String PASS = "pwd";
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        userName = servletRequest.getParameter("username");
+        password = servletRequest.getParameter("password");
+        servletRequest.setAttribute(USE, userName);
+        servletRequest.setAttribute(PASS, password);
+
+        servletContext = servletRequest.getServletContext();
+        RequestDispatcher requestDispatcher = servletContext.getRequestDispatcher(tagjsp);
+        requestDispatcher.forward(servletRequest, servletResponse);
+    }
+}
